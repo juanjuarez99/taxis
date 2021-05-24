ed8bc4a1a4d14d794d22b53b2f6505f1c81a7cb7
LadyScream <the.lady.scream@gmail.com>
2021-05-24 04:46:55 -0500

    Added 'login' route

---
+++ rutas/login.js
@@ -0,0 +1,18 @@
+module.exports = {
+	setup: (app, connection) => {
+		const route = "/login";
+		app.post(route, (req, res) => {
+			const { usuario, contra } = req.body;
+			connection.query(
+				`SELECT * FROM usuarios WHERE usuario = '${usuario}' AND contra = '${contra}'`,
+				(error, result) => {
+					if (error) {
+						res.json(error);
+						return;
+					}
+					res.json(result);
+				}
+			);
+		});
+	},
+};
